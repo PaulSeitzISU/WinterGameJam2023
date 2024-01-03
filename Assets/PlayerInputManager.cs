90750b0696a66e08252a7b9ccb798902c210da3c
ToMeHarris <34361639+ToMeHarris@users.noreply.github.com>
2024-01-03 17:51:25 -0600

    fixed compilation errors

--- Assets/PlayerInputManager.cs
+++ Assets/PlayerInputManager.cs
@@ -4,7 +4,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
