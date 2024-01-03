54c0d0f0d4953aa074ed12ac3e1960dae2d69d3b
Txhowar <77594787+Txhowar@users.noreply.github.com>
2024-01-03 14:44:03 -0600

    Win/Lose/Main Menu Scenes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -15,4 +15,10 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void ReturnMain()
+    {
+      //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+       SceneManager.LoadScene("MainMenu");
+    }
 }
