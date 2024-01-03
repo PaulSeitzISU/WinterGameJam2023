bb4dc8f1d3e41a408501ed5e5ca2701dc72a9347
ToMeHarris <34361639+ToMeHarris@users.noreply.github.com>
2024-01-03 06:47:59 -0600

    spawning

--- Assets/Scripts/Level/LevelPopulator.cs
+++ Assets/Scripts/Level/LevelPopulator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -12,22 +13,31 @@ public class LevelPopulator : MonoBehaviour
     public GameObject slimePickupPrefab;
     public GameObject[] enemyPrefabs;
 
-    public EnemyManager enemyManager;
+    public int maxEnemiesPerRoom;
+    public float slimePickupChance;
 
-    public GameObject player;
+    public EnemyManager enemyManager;
 
     public void PopulateLevel(int scaleFactor, List<Partition> rooms)
     {
         List<Partition> toProcess = new List<Partition>(rooms);
-        Partition startRoom = GetRandomAndRemove(rooms);
-        enemyManager.PlayerList = new GameObject[] { PlaceInRoom(playerPrefab, startRoom, scaleFactor) };
+        Partition startRoom = GetRandomAndRemove(toProcess);
+        // Spawn mr. player
+        enemyManager.PlayerList = new GameObject[] { PlaceInRoom(playerPrefab, startRoom, scaleFactor, -1, new HashSet<Vector2Int>()) };
 
         List<GameObject> spawnedEnemies = new List<GameObject>();
-        int numRooms = toProcess.Count;
-        for (int i = 0; i < numRooms; i++)
+        foreach (var _ in Enumerable.Range(0, toProcess.Count))
         {
-            Partition room = GetRandomAndRemove(rooms);
-            spawnedEnemies.Add(PlaceInRoom(enemyPrefabs[0], room, scaleFactor));
+            Partition room = GetRandomAndRemove(toProcess);
+            HashSet<Vector2Int> occupied = new HashSet<Vector2Int>();
+            
+            // Spawn up to maxEnemiesPerRoom enemies
+            foreach (var __ in Enumerable.Range(0, Random.Range(0, maxEnemiesPerRoom)))
+                spawnedEnemies.Add(PlaceInRoom(enemyPrefabs[Random.Range(0, enemyPrefabs.Length)], room, scaleFactor, -1, occupied));
+
+            // Chance to spawn a pickup
+            if (Random.value < slimePickupChance)
+                PlaceInRoom(slimePickupPrefab, room, scaleFactor, -0.5f, occupied);
         }
     }
 
@@ -39,22 +49,27 @@ public class LevelPopulator : MonoBehaviour
         return partition;
     }
 
-    private GameObject PlaceInRoom(GameObject prefab, Partition room, int scaleFactor)
+    private GameObject PlaceInRoom(GameObject prefab, Partition room, int scaleFactor, float zPos, HashSet<Vector2Int> occupied)
     {
         Vector2Int scaledPos = new Vector2Int(room.pos.x * scaleFactor, room.pos.y * scaleFactor);
         // Scaled room size (accounting for side wall tiles)
         Vector2Int scaledSize = new Vector2Int(((room.size.x - 1) * scaleFactor) - 1, (room.size.y - 1) * scaleFactor);
 
-        for (int i = scaledPos.x; i < scaledPos.x + scaledSize.x; i++)
-            for (int j = scaledPos.y; j < scaledPos.y + scaledSize.y; j++)
-                debugTilemap.SetTile(new Vector3Int(j, i, 0), debugTile);
+        /*        for (int i = scaledPos.x; i < scaledPos.x + scaledSize.x; i++)
+                    for (int j = scaledPos.y; j < scaledPos.y + scaledSize.y; j++)
+                        debugTilemap.SetTile(new Vector3Int(j, i, 0), debugTile);*/
 
         // Get random position in room
-        Vector2Int spawnPosition = new Vector2Int(
-            Random.Range(scaledPos.x, scaledPos.x + scaledSize.x),
-            Random.Range(scaledPos.y, scaledPos.y + scaledSize.y)
-        );
-
-        return GameObject.Instantiate(prefab, new Vector3(spawnPosition.y + 0.5f, spawnPosition.x + 0.5f, -1), Quaternion.identity);
+        Vector2Int spawnPosition;
+        do
+        {
+            spawnPosition = new Vector2Int(
+                Random.Range(scaledPos.x, scaledPos.x + scaledSize.x),
+                Random.Range(scaledPos.y, scaledPos.y + scaledSize.y)
+            );
+        } while (occupied.Contains(spawnPosition));
+        occupied.Add(spawnPosition);
+        
+        return GameObject.Instantiate(prefab, new Vector3(spawnPosition.y + 0.5f, spawnPosition.x + 0.5f, zPos), Quaternion.identity);
     }
 }
